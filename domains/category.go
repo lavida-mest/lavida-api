b81a43623e9a815e9d765787aface73c1b9f3058
muathendirangu <muathe.ndirangu@gmail.com>
2020-02-22 10:56:28 +0000

    add Response object to format the payload

--- domains/category.go
+++ domains/category.go
@@ -1,12 +1,28 @@
 package domains
 
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
+
+//Response formats the returned payload
+type Response struct {
+	Success   bool        `json:"success:omitempty"`
+	Message   string      `json:"message,omitempty"`
+	Timestamp time.Time   `json:"timestamp,omitempty"`
+	Errors    interface{} `json:"errors,omitempty"`
+	Payload   interface{} `json:"payload,omitempty"`
+}
+
 //Category of trip
 type Category struct {
 	ID   int64  `json:"category_id"`
 	Name string `json:"category_name"`
+	gorm.Model
 }
 
 //Repository defines how to store category of
 type Repository interface {
-	Store(c *Category) error
+	Store(name string) Response
 }
