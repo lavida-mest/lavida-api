3137d4b3a2df2fde8a3f55a99b90398284efa190
muathendirangu <muathe.ndirangu@gmail.com>
2020-02-23 22:57:39 +0000

    implement the Get method of repository interface

--- guide/mysql/guide.go
+++ guide/mysql/guide.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"log"
 
 	"github.com/muathendirangu/lavida-api/guide"
 )
@@ -30,4 +31,30 @@ func (r *repository) Store(guide *guide.Guide) error {
 	lasTG, err := res.LastInsertId()
 	guide.ID = lasTG
 	return nil
+}
+
+func (r *repository) Get() []*guide.Guide {
+	query := `SELECT * FROM guide`
+	rows, err := r.conn.Query(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var guides = make([]*guide.Guide, 0)
+
+	for rows.Next() {
+		guide := new(guide.Guide)
+		err := rows.Scan(
+			&guide.ID,
+			&guide.Name,
+			&guide.Email,
+			&guide.Number,
+			&guide.Category,
+		)
+		if err != nil {
+			log.Fatal(err)
+		}
+		guides = append(guides, guide)
+	}
+	defer rows.Close()
+	return guides
 }
