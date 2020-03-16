012b8eae213b9941c08dcae2926f610d943573f3
muathendirangu <muathe.ndirangu@gmail.com>
2020-03-16 19:20:08 +0000

    format sql query

--- guide/mysql/guide.go
+++ guide/mysql/guide.go
@@ -19,7 +19,13 @@ func New(conn *sql.DB) guide.Repository {
 }
 
 func (r *repository) Store(guide *guide.Guide) error {
-	query := `INSERT INTO guide (tour_guide_name, tour_guide_email, tour_guide_number, category_id) VALUES(?,?,?,?)`
+	query := `INSERT INTO guide(
+						tour_guide_name,
+						tour_guide_email,
+						tour_guide_number,
+						category_id
+					)
+					VALUES(?, ?, ?, ?)`
 	stmt, err := r.conn.Prepare(query)
 	if err != nil {
 		return err
